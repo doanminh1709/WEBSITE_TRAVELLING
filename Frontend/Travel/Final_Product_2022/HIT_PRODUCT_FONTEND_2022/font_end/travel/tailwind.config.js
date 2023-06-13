b022eb1a618785ddb63ca470108da42077929b89
doanminh1709 <doanducminh11082002@gmail.com>
2023-06-14 00:18:24 +0700

    Frontend

---
+++ Frontend/Travel/Final_Product_2022/HIT_PRODUCT_FONTEND_2022/font_end/travel/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      sm: '400px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '740px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+};
