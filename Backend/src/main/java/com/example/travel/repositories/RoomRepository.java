f2a8968d3ec7023fb6757bc79c6871fed814abcc
khoinguyentb <Vankhoi06102002@gmail.com>
2023-06-14 22:37:56 +0700

    push code l1

--- Backend/src/main/java/com/example/travel/repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travel.repositories;
-
-import com.example.travel.daos.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
--- Backend/src/main/java/com/example/travel/repositories/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travel.repositories;
-
-import com.example.travel.daos.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
